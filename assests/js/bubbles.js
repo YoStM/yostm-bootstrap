886195a21760b3c7654ea25875681ca9ae7f40c6
YoStM <yo.stm@outlook.com>
2021-07-10 23:51:21 +0200

    add few javascript tweaks

--- assests/js/bubbles.js
+++ assests/js/bubbles.js
@@ -3,26 +3,71 @@ const colors = [
     "#4285F4",
     "#DB4437",
     "#F4B400",
-    "#0F9D59"
+    "#0F9D59",
+    "#dddfd4",
+    "#fae596",
+    "#3fb0ac",
+    "#173e43",
+    "#c2dde6",
+    "#e05915"
 ];
-bubbles[1].setAttribute('style', 'border-color: ' + colors[0] + ';' + 'background-color: ' + colors[0] + ';');
-bubbles[5].setAttribute('style', 'border-color: ' + colors[1] + ';' + 'background-color: ' + colors[1] + ';');
-bubbles[6].setAttribute('style', 'border-color: ' + colors[3] + ';' + 'background-color: ' + colors[3] + ';');
-bubbles[9].setAttribute('style', 'border-color: ' + colors[2] + ';' + 'background-color: ' + colors[2] + ';');
-first_bubble_index = setInterval(setFirstBubbleIndex, 19000);
-second_bubble_index = setInterval(setSecondBubbleIndex, 19000);
-setInterval(changebubbleColor, 19000);
+bubbles[0].setAttribute('style', 'border-color: ' + colors[0] + '; ' + 'background: ' + colors[0] + ';');
+bubbles[4].setAttribute('style', 'border-color: ' + colors[1] + '; ' + 'background: ' + colors[1] + ';');
+bubbles[5].setAttribute('style', 'border-color: ' + colors[2] + '; ' + 'background: ' + colors[2] + ';');
+bubbles[6].setAttribute('style', 'border-color: ' + colors[3] + ';'  + 'background: ' + colors[3] + ';');
+
+setInterval(changebubbleColor, 15000);
+
 function changebubbleColor() {
-    bubbles[first_bubble_index].setAttribute('style', 'border-color: ' + colors[0] + ';' + ' background-color: ' + colors[0] + ';');
-    bubbles[second_bubble_index].setAttribute('style', 'border-color: ' + colors[1] + ';' + ' background-color: ' + colors[1] + ';');
-    bubbles[first_bubble_index - 1].setAttribute('style', 'border-color: ' + colors[3] + ';' + 'background-color: ' + colors[3] + ';');
-    bubbles[second_bubble_index - 1].setAttribute('style', 'border-color: ' + colors[2] + ';' + 'background-color: ' + colors[2] + ';');
+
+    var bubble_indexes = setRandom_bubbleIndexes();
+    var color_indexes = setRandom_colorIndexes();
+    resetInlineFormating();
+    for(let i = 0; i < bubble_indexes.length; i++) {
+        bubble_index = bubble_indexes[i]
+        addInlineFormating(bubble_index, i);
+    }
+
 }
-function setFirstBubbleIndex() {
-    bubble_index = Math.floor(Math.random() * 10) + 1;
-    return bubble_index;
+
+function deleteInlineFormating(bubble_index) {
+    bubbles[bubble_index].removeAttribute('style');
 }
-function setSecondBubbleIndex() {
-    bubble_index = Math.floor(Math.random() * 10) + 1;
-    return bubble_index;
+
+function addInlineFormating(bubble_index, color_index) {
+    bubbles[bubble_index].setAttribute('style', 'border-color: ' + colors[color_index] + '; ' + 'background: ' + colors[color_index] + ';');
+}
+
+function resetInlineFormating() {
+    for (let i=0; i<bubbles.length; i++) {
+        if (bubbles[i].hasAttribute('style')){
+            deleteInlineFormating(i);
+        }
+    }
+}
+
+function setRandom_bubbleIndexes() {
+    let random_indexes = [];
+    bubble_index = bubbles[Math.floor(Math.random()*bubbles.length)];
+    if (bubble_index === bubbles[0]) {
+        random_indexes = [0,3,4,8,9];
+    } else if (bubble_index <= 5) {
+        random_indexes = [1,2,5,6,7];
+    } else {
+        random_indexes = [2,3,5,6,9];
+    }
+    return random_indexes;
+}
+
+function setRandom_colorIndexes() {
+    let random_indexes = [];
+    color_index = colors[Math.floor(Math.random()*colors.length)];
+    if (color_index === bubbles[0]) {
+        random_indexes = [0,3,4,8,9];
+    } else if (color_index <= 5) {
+        random_indexes = [1,2,5,6,7];
+    } else {
+        random_indexes = [2,3,5,6,9];
+    }
+    return random_indexes.reverse;
 }
