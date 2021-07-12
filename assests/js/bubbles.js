5bef2fe3639da142b4473dc770fe7b730ff8fa98
YoStM <yo.stm@outlook.com>
2021-07-12 17:02:06 +0200

    Add last hero background improvments

--- assests/js/bubbles.js
+++ assests/js/bubbles.js
@@ -11,23 +11,31 @@ const colors = [
     "#c2dde6",
     "#e05915"
 ];
-bubbles[0].setAttribute('style', 'border-color: ' + colors[0] + '; ' + 'background: ' + colors[0] + ';');
-bubbles[4].setAttribute('style', 'border-color: ' + colors[1] + '; ' + 'background: ' + colors[1] + ';');
-bubbles[5].setAttribute('style', 'border-color: ' + colors[2] + '; ' + 'background: ' + colors[2] + ';');
-bubbles[6].setAttribute('style', 'border-color: ' + colors[3] + ';'  + 'background: ' + colors[3] + ';');
 
-setInterval(changebubbleColor, 15000);
+setFirstBubbleColor();
 
-function changebubbleColor() {
+setInterval(changeBubbleColor, 29000);
 
-    var bubble_indexes = setRandom_bubbleIndexes();
+function setFirstBubbleColor() {
+    var bubble_indexes = [1, 2, 4, 7, 8];
     var color_indexes = setRandom_colorIndexes();
-    resetInlineFormating();
-    for(let i = 0; i < bubble_indexes.length; i++) {
-        bubble_index = bubble_indexes[i]
-        addInlineFormating(bubble_index, i);
+    for (let i = 0; i < 5; i++) {
+        bubble_index = bubble_indexes[i];
+        color_index = color_indexes[i];
+        addInlineFormating(bubble_index, color_index);
     }
 
+}
+
+function changeBubbleColor() {
+    var bubble_indexes = [1, 2, 4, 7, 8];
+    var color_indexes = setRandom_colorIndexes();
+
+    for (let i = 0; i < 5; i++) {
+        bubble_index = bubble_indexes[i];
+        color_index = color_indexes[i];
+        addInlineFormating(bubble_index, color_index);
+    }
 }
 
 function deleteInlineFormating(bubble_index) {
@@ -38,36 +46,72 @@ function addInlineFormating(bubble_index, color_index) {
     bubbles[bubble_index].setAttribute('style', 'border-color: ' + colors[color_index] + '; ' + 'background: ' + colors[color_index] + ';');
 }
 
-function resetInlineFormating() {
-    for (let i=0; i<bubbles.length; i++) {
-        if (bubbles[i].hasAttribute('style')){
-            deleteInlineFormating(i);
-        }
-    }
-}
+// function resetInlineFormating() {
+//     for (let i = 0; i < bubbles.length; i++) {
+//         if (bubbles[i].hasAttribute('style')) {
+//             deleteInlineFormating(i);
+//         }
+//     }
+// }
 
-function setRandom_bubbleIndexes() {
-    let random_indexes = [];
-    bubble_index = bubbles[Math.floor(Math.random()*bubbles.length)];
-    if (bubble_index === bubbles[0]) {
-        random_indexes = [0,3,4,8,9];
-    } else if (bubble_index <= 5) {
-        random_indexes = [1,2,5,6,7];
-    } else {
-        random_indexes = [2,3,5,6,9];
-    }
-    return random_indexes;
-}
+// 1: 12s & 2s delay, 
+// 2: 19s & 4s delay,
+// 4: 19s & 0s delay,
+// 7: 45s & 15s delay, 
+// 8: 35s & 2s delay
+// function setRandom_bubbleIndexes() {
+//     let random_indexes = [];
+//     bubble_index = bubbles[Math.floor(Math.random() * bubbles.length)];
+//     if (bubble_index === bubbles[0]) {
+//         random_indexes = [0, 3, 4, 8, 9];
+//     } else if (bubble_index <= 5) {
+//         random_indexes = [1, 2, 5, 6, 7];
+//     } else {
+//         random_indexes = [2, 3, 5, 6, 9];
+//     }
+//     return random_indexes;
+// }
+// function setBubbleIndexes() {
+//     const bubble_indexes = [1, 2, 4, 7, 8];
+//     return bubble_indexes;
+// }
 
 function setRandom_colorIndexes() {
     let random_indexes = [];
-    color_index = colors[Math.floor(Math.random()*colors.length)];
-    if (color_index === bubbles[0]) {
-        random_indexes = [0,3,4,8,9];
-    } else if (color_index <= 5) {
-        random_indexes = [1,2,5,6,7];
-    } else {
-        random_indexes = [2,3,5,6,9];
+    color_index = Math.floor(Math.random() * colors.length);
+    switch (color_index) {
+        case 0:
+            random_indexes = [1, 4, 6, 9, 0];
+            break;
+        case 1:
+            random_indexes = [2, 5, 7, 3, 1];
+            break;
+        case 2:
+            random_indexes = [3, 8, 6, 5, 2];
+            break;
+        case 3:
+            random_indexes = [4, 2, 7, 5, 3];
+            break;
+        case 4:
+            random_indexes = [5, 9, 8, 1, 4];
+            break;
+        case 5:
+            random_indexes = [6, 2, 3, 9, 5];
+            break;
+        case 6:
+            random_indexes = [7, 5, 1, 2, 6];
+            break;
+        case 7:
+            random_indexes = [8, 3, 4, 6, 7];
+            break;
+        case 8:
+            random_indexes = [9, 7, 5, 4, 8];
+            break;
+        case 9:
+            random_indexes = [0, 2, 6, 3, 9];
+            break;
     }
-    return random_indexes.reverse;
+    console.log(color_index);
+    console.log(random_indexes);
+    return random_indexes;
 }
